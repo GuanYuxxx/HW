ee77a2d78148da4c5477f8e2c99b3af8dd4a3064
ChenGuanYu123 <a057923062@gmail.com>
2022-10-23 05:41:58 +0800

    feat:design pattern

--- src/factory/D2.java
+++ src/factory/D2.java
@@ -16,7 +16,7 @@ public class D2 {
     public static void main(String[] args) {
         Factory friesFac = new FrenchFriesFactory();
         Product fries = friesFac.getProduct();
-        Product myfries = ((FrenchFriesFactory) friesFac).getProduct("無鹽");
+        Product myfries = ((FrenchFriesFactory) friesFac).getProduct("無鹽的");
 
         fries.describe();//我是有鹽巴薯條
         myfries.describe();//我是無鹽的薯條
